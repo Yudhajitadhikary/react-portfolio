346588c4e9eea81a19e227f870c8ab19c77a47d1
yudhajit <yudhajitadhikary@gmail.com>
2024-04-21 07:00:33 +0530

    Added fixs

--- src/components/Header/DesktopNavigation.js
+++ src/components/Header/DesktopNavigation.js
@@ -5,10 +5,10 @@ const DesktopNavigation=()=>{
     return(
         <div data-collapse="small" data-animation="default" data-duration="400" id="navbar-desktop" className="navbar-desktop">
             <nav role="navigation" className="nav-menu-2">
-                <a className={styles.anchorLink} href="/"> Home</a>
-                <a className={styles.anchorLink} href="/about">About</a>
+                <a className={styles.anchorLink} href="/react-portfolio"> Home</a>
+                <a className={styles.anchorLink} href="/react-portfolio/about">About</a>
                 {/* <a className={styles.anchorLink} href="/works" id="navbar-building">Work</a> */}
-                <a className={styles.anchorLink} href="/blogs">Blogs</a>
+                <a className={styles.anchorLink} href="/react-portfolio/blogs">Blogs</a>
                 {/* <a className={styles.anchorLink} href="/contact" aria-current="page">Contact</a> */}
                 </nav>
         </div>
